cf3aaf5938370182305ed281100d7ca0e37758f0
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-04-20 12:51:52 -0700

    first commit

---
+++ database/migrations/2024_03_06_122557_create_camp_grounds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('camp_grounds', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('country');
+            $table->string('city');
+            $table->string('region');
+            $table->integer('cm_type')->default("0");//  جبل-0 - صحراء-1 -   -2 - غابة
+
+            $table->integer('cm_season')->default("3");// spring 1 - summer  2 - fall 3 - winter 0
+
+            $table->string('campGround_image',300)->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('camp_grounds');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Http\Controllers\ArmanController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CampGroundController;
+
+
+
+
+
+Route::get('/', function () {
+    return view('frontend.index');
+});
+
+Route::get('/n', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
+/////////////////////////////////////---CampGround
+
+/*
+Route::get('/campground', function () {
+    return view('backend.campGround');
+})->middleware(['auth', 'verified'])->name('campground');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/campground', [CampgroundController::class, 'index'])->name('campground');
+
+});
+
+*/
+#-------------camping grounds
+
+Route::middleware(['auth'])->group(function () {
+
+    //Route::get('/campground1', [CampgroundController::class, 'index'])->name('campground1');
+
+    Route::resource('/adminpanel/campground', CampGroundController::class);
+
+   // Route::resource('/campground', CampGroundController::class);
+
+});
+
+
+#-------------Arman routes
+
+Route::middleware(['auth'])->group(function () {
+
+
+    Route::resource('/adminpanel/arman', ArmanController::class);
+
+});
