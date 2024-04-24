9fc21f97898794c24eee64658a8613d0b5313098
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-04-24 08:54:35 -0700

    add new linkes to home

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Bright night</title>
+    <title>Armenia Tourism</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -33,45 +33,11 @@
 </head>
 
 <body>
-    <!-- Spinner Start
-    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 30rem;" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
--->
-    <!-- Spinner End -->
-
-
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-dark px-5 d-none d-lg-block">
-        <div class="row gx-0">
-            <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
-                <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York, USA</small>
-                    <small class="me-3 text-light"><i class="fa fa-phone-alt me-2"></i>+012 345 6789</small>
-                    <small class="text-light"><i class="fa fa-envelope-open me-2"></i>info@example.com</small>
-                </div>
-            </div>
-            <div class="col-lg-4 text-center text-lg-end">
-                <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i class="fab fa-twitter fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i class="fab fa-facebook-f fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i class="fab fa-linkedin-in fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle me-2" href=""><i class="fab fa-instagram fw-normal"></i></a>
-                    <a class="btn btn-sm btn-outline-light btn-sm-square rounded-circle" href=""><i class="fab fa-youtube fw-normal"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Topbar End -->
-
-
-    <!-- Navbar & Hero Start -->
+        <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
             <a href="" class="navbar-brand p-0">
-                <h1 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>Bright night</h1>
+                <h1 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>Armenia Tourism</h1>
                 <!-- <img src="img/logo.png" alt="Logo"> -->
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -82,18 +48,27 @@
                     <a href="index.html" class="nav-item nav-link active">Home</a>
                     <a href="about.html" class="nav-item nav-link">About</a>
                     <a href="service.html" class="nav-item nav-link">Services</a>
-                    <a href="package.html" class="nav-item nav-link">Packages</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">your distination</a>
                         <div class="dropdown-menu m-0">
-                            <a href="destination.html" class="dropdown-item">Destination</a>
-                            <a href="booking.html" class="dropdown-item">Booking</a>
-                            <a href="team.html" class="dropdown-item">Travel Guides</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="404.html" class="dropdown-item">404 Page</a>
+                            <a href="destination.html" class="dropdown-item">castles</a>
+                            <a href="booking.html" class="dropdown-item">caves</a>
+                            <a href="team.html" class="dropdown-item">springs</a>
+                            <a href="testimonial.html" class="dropdown-item">churches</a>
+                            <a href="404.html" class="dropdown-item">distances</a>
+
+                            <a href="destination.html" class="dropdown-item">forests</a>
+                            <a href="booking.html" class="dropdown-item">gardens</a>
+                            <a href="team.html" class="dropdown-item">lakes</a>
+                            <a href="testimonial.html" class="dropdown-item">malls</a>
+                            <a href="404.html" class="dropdown-item">mountains</a>
+
+                            <a href="destination.html" class="dropdown-item">museums</a>
+                            <a href="team.html" class="dropdown-item">slopes</a>
+                            <a href="testimonial.html" class="dropdown-item">threaters</a>
+                            <a href="404.html" class="dropdown-item">campgrounds</a>
                         </div>
                     </div>
-                    <a href="contact.html" class="nav-item nav-link">Contact</a>
                 </div>
                 <a href="/login" class="btn btn-primary rounded-pill py-2 px-4" style="margin-right: 5px">Login </a>
                 <a href="/register" class="btn btn-primary rounded-pill py-2 px-4">Register </a>
@@ -105,7 +80,7 @@
                 <div class="row justify-content-center py-5">
                     <div class="col-lg-10 pt-lg-5 mt-lg-5 text-center">
                         <h1 class="display-3 text-white mb-3 animated slideInDown">Enjoy Your Vacation With Us</h1>
-                        <p class="fs-4 text-white mb-4 animated slideInDown">"Discover new places around the world and enjoy a wonderful camping experience in complete safety and an environment full of adventures and natural beauty. An unforgettable experience awaits you!"</p>
+                        <p class="fs-4 text-white mb-4 animated slideInDown">"Discover new places around the Armenia and enjoy a wonderful camping experience in complete safety and an environment full of adventures and natural beauty. An unforgettable experience awaits you!"</p>
                         <div class="position-relative w-75 mx-auto animated slideInDown">
                             <input class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Eg: Thailand">
                             <button type="button" class="btn btn-primary rounded-pill py-2 px-4 position-absolute top-0 end-0 me-2" style="margin-top: 7px;">Search</button>
