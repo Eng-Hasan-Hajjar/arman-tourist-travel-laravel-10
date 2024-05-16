28d11a997652d23e0cef20295c18675850c069a2
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-16 06:42:52 -0700

    update arman view

--- resources/views/backend/arman/show.blade.php
+++ resources/views/backend/arman/show.blade.php
@@ -3,11 +3,14 @@
    info
 @endsection
 @section('content')
-    <div class="jumbotron text-center">
 
+    <div class="jumbotron text-center">
+        <div  style="width: 335px; margin-left: 335px ;margin-bottom: 20px">
+            <a href="{{ route('arman.index') }}" class="btn btn-primary">  back </a>
+        </div>
         <br />
 
-        <h3>Name : {{ $data->name }} </h3>
+        <h3> Name : {{ $data->name }} </h3>
         <h3>description : {{ $data->description }}</h3>
         <h3>location : {{ $data->location }}</h3>
         <h3>airport : {{ $data->airport }} </h3>
@@ -16,7 +19,5 @@
 
 
     </div>
-    <div  style="width: 335px; margin-left: 335px ;margin-bottom: 20px">
-        <a href="{{ route('arman.index') }}" class="btn btn-default">  back </a>
-    </div>
+
 @endsection
