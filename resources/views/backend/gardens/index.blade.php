e0402e13dda2f5fdea23125ea613889683cae2d4
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-17 16:07:09 -0700

    create many  views for all things for forests  controller

---
+++ resources/views/backend/gardens/index.blade.php
@@ -0,0 +1,108 @@
+@extends('admin.layouts.layout')
+
+@section('title')
+التحكم
+@endsection
+
+@section('header')
+    <!-- DataTables -->
+    {{ Html::style('admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}
+    {{ Html::style('admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}
+@endsection
+
+@section('content')
+
+
+                <div class="container hcontainer"  >
+                    <div class="card hcard helement hcard-body" >
+
+                        <div class="card-header"><p  class="float-left">all gardens </p></div>
+                        <div class="card-header">
+                            <a href="{{ route('caves.create') }}" class=" btn btn-success float-right">create new </a>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="data" class="table table-bordered table-hover">
+                                <thead>
+
+                                    <tr>
+                                        <th>Arman</th>
+                                        <th>name</th>
+                                        <th>description</th>
+                                        <th>location</th>
+                                        <th>date</th>
+
+
+                                        <th>image</th>
+
+
+                                        <th>control</th>
+                                        <th>del</th>
+                                    </tr>
+                                </thead>
+
+
+
+                                <tbody>
+
+
+                                    @foreach ($caves as $row)
+                                        <tr>
+                                            @foreach ($armans as $arman )
+                                               @if( $row->arman_id ==  $arman->id)
+                                                  <td>{{  $arman->name }}</td>
+                                               @endif
+                                            @endforeach
+
+
+                                            <td>{{ $row->name }}</td>
+                                            <td>{{ $row->description }}</td>
+                                            <td>{{ $row->location }}</td>
+                                            <td>{{ $row->date }}</td>
+
+                                            <td><img src="{{ URL::to('/') }}/images/{{ $row->image }}"
+                                                    class="img-thumbnail" width="75" />
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('caves.show', $row->id) }}"
+                                                    class="btn btn-primary">Show</a>
+                                                <a href="{{ route('caves.edit', $row->id) }}"
+                                                    class="btn btn-warning">Edit</a>
+                                            </td>
+
+                                            <td>
+
+                                                <form method="post" class="delete_form"
+                                                    action="{{ route('caves.destroy', $row->id) }}">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="_method" value="DELETE" />
+                                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this record?');" >Delete</button>
+                                                </form>
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
+
+
+                                </tbody>
+
+
+                            </table>
+                            {!! $caves->links() !!}
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+
+                </div>
+                <!-- /.col -->
+
+
+@endsection
+
+@section('footer')
+    <!-- DataTables -->
+    {{ Html::script('admin/plugins/datatables/jquery.dataTables.min.js') }}
+    {{ Html::script('admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}
+    {{ Html::script('admin/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}
+    {{ Html::script('admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}
+@endsection
---
+++ resources/views/backend/museums/show.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layouts.layout')
+
+@section('title')
+   Cave Information
+@endsection
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h3>Cave Information</h3>
+                </div>
+                <div class="card-body">
+                    <div class="text-center mb-4">
+                        <a href="{{ route('caves.index') }}" class="btn btn-primary">Back</a>
+                    </div>
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Arman:</th>
+                            <td>{{ $arman->name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Name:</th>
+                            <td>{{ $data->name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Description:</th>
+                            <td>{{ $data->description }}</td>
+                        </tr>
+                        <tr>
+                            <th>Location:</th>
+                            <td>{{ $data->location }}</td>
+                        </tr>
+                        <tr>
+                            <th>Date:</th>
+                            <td>{{ $data->date }}</td>
+                        </tr>
+                        <tr>
+                            <th>Image:</th>
+                            <td>
+                                <img src="{{ URL::to('/') }}/images/{{ $data->image }}" class="img-thumbnail" style="width: 300px; height: auto;" />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
