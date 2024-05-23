34d46e4f1bb318aa2567f1be5632b20d39815af7
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-22 18:10:03 -0700

    alter views for front end

--- resources/views/frontend/destination.blade.php
+++ resources/views/frontend/destination.blade.php
@@ -2,46 +2,16 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Armenia Tourism</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    @include('frontend.layouts.head')
 
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-     <!-- Libraries Stylesheet -->
-     <link href="frontendAsset/lib/animate/animate.min.css" rel="stylesheet">
-     <link href="frontendAsset/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-     <link href="frontendAsset/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
-
-     <!-- Customized Bootstrap Stylesheet -->
-     <link href="frontendAsset/css/bootstrap.min.css" rel="stylesheet">
-
-     <!-- Template Stylesheet -->
-     <link href="frontendAsset/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-
-
-
-
+    @include('frontend.layouts.spinner')
     <!-- Navbar & Hero Start -->
     <div class="container-fluid position-relative p-0">
 
         @include('frontend.layouts.nav')
-
 
         <div class="container-fluid bg-primary py-5 mb-5 hero-header">
             <div class="container py-5">
@@ -60,8 +30,6 @@
         </div>
     </div>
     <!-- Navbar & Hero End -->
-
-
     <!-- Destination Start -->
     <div class="container-xxl py-5 destination">
         <div class="container">
@@ -183,17 +151,10 @@
 <!-- Destination Start -->
 
 
-
-
-
-
     @include('frontend.layouts.footer')
-
-
 
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-
 
     @include('frontend.layouts.javascript')
 
